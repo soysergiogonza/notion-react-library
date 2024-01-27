176e14c01eb538c66a724ad0172dc38121377089
Sergio González Sánchez <soysergiogonzalez@outlook.es>
2024-01-27 12:57:52 -0500

    [FIX] cover styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import styles from './page.module.css';
-import {Cover, IconPage} from '../components';
+import {Cover} from '../components';
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<Cover coverSource="/notion-react-library.svg" style={{display: 'block', width: '100px', height: '100px'}}/>
-			<picture style={{display: 'block', width: '100px', height: '100px'}}>
-				<IconPage iconSource="notion-react-library_icon.svg"/>
-			</picture>
-		
+			<section>
+					<Cover coverSource="/notion-react-library.svg" />
+			</section>
 		</main>
 	);
 }
--- src/components/Cover/index.tsx
+++ src/components/Cover/index.tsx
@@ -6,7 +6,9 @@ import styles from './Cover.module.css';
 
 export const Cover = ({coverSource, style}: CoverSource) => {
 	return (
+		<picture className={styles.picture} style={style}>
 		<Image src={coverSource} alt="cover" width={100}
-		       height={100} className={styles.cover} style={style}/>
+		       height={100} className={styles.cover} />
+		</picture>
 	);
 };
